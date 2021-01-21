54d87e98357868a144ce162af37489b43d00d8ab
andrenguyener <andrenguyenp@gmail.com>
2021-01-21 13:24:21 -0800

    remove console message

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const ScrollToTop: React.FC<{ pathName: string }> = ({ children, pathName }) => {
-    console.warn(pathName);
     React.useEffect(() => window.scrollTo(0, 0), [pathName]);
     return <>{children}</>;
 };
