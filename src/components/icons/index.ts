4c61b4a5000bfd0675607128103d62a6feea7a4d
andrenguyener <andrenguyenp@gmail.com>
2021-01-21 16:33:06 -0800

    Convert icon components to .tsx

---
+++ src/components/icons/index.ts
@@ -0,0 +1,36 @@
+import IconExternal from "./external";
+import IconGithub from "./github";
+import IconInfo from "./info";
+import IconLoader from "./loader";
+import IconMicrophone from "./microphone";
+import IconMusic from "./music";
+import IconPlaylist from "./playlist";
+import IconSpotify from "./spotify";
+import IconTime from "./time";
+import IconUser from "./user";
+
+export {
+    IconUser,
+    IconGithub,
+    IconExternal,
+    IconSpotify,
+    IconLoader,
+    IconTime,
+    IconMicrophone,
+    IconPlaylist,
+    IconMusic,
+    IconInfo,
+};
+
+export default {
+    IconUser,
+    IconGithub,
+    IconExternal,
+    IconSpotify,
+    IconLoader,
+    IconTime,
+    IconMicrophone,
+    IconPlaylist,
+    IconMusic,
+    IconInfo,
+};
