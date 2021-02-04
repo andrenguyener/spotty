da367ec824c7becf129d42db62cf8b00ef5dfdc9
andrenguyener <andrenguyenp@gmail.com>
2021-02-03 16:08:07 -0800

    Attempt #2 to adjust next config transpile module

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ const withTM = require("next-transpile-modules")([
     "siriwave"
   ]);
   
-  module.exports = withTM();
+  module.exports = withTM({
+    // Target must be serverless
+    target: 'experimental-serverless-trace'
+  });
